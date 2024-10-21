b5a2ede60a092e19470da21365f700fd7e135d41
soumyaswe <soumya24work@gmail.com>
2024-10-21 22:28:11 +0530

    updated

--- Star/4.py
+++ Star/4.py
@@ -1,4 +1,7 @@
-for i in range(1,7) :
-    for j in range(1,i+1) :
-        print("*", end=" ")
-    print(end="\n")
+row=6
+for i in range(1,row+1):
+    print("*"*i,end="\n")
+print()
+
+for i in range(row,0,-1):
+    print("*"*i,end="\n")
