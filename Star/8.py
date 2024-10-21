4fee093f7cef6cf4471f00ced5386ea3d1ad2fcf
soumyaswe <soumya24work@gmail.com>
2024-10-21 21:38:22 +0530

    done

--- Star/8.py
+++ Star/8.py
@@ -1,11 +1,11 @@
-r = 8
-c = 2*r
-
-for i in range(1,r+1) :
-    for j in range(1,c+1) :
-        if(j <= (c/2 - i) and j >= (c/2 + i-1)) :
-            print("*", end="")
-        else :
-            print("_", end="")
-    print(end="\n")
-afs
+r = 14
+print("*" * r, end="\n")
+i = (r // 2) - 1
+j = 2
+while(i != 0) :
+    while(j <= (r - 2)) :
+        print("*" * i, end="")
+        print("_" * j, end="")
+        print("*" * i, end="\n")
+        i -= 1
+        j += 2
